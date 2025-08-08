a820d238e3c89b77c35df4f9014d6bac7e98691c
YashBarbole <yashbarbole7110@gmail.com>
2025-08-09 01:17:33 +0530

    wrapperclass

---
+++ WrapperExample.java
@@ -0,0 +1,15 @@
+public class WrapperExample {
+    public static void main(String[] args) {
+        int a=10;//primitive 
+        int b=20;
+        Integer num= 12;//object
+
+        
+    }
+
+   public static void swap(int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+}
