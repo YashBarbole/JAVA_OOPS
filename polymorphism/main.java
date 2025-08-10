b99df76433b4d5c068a73692d4b9cdcf0bf8cad5
YashBarbole <yashbarbole7110@gmail.com>
2025-08-10 17:04:48 +0530

    poly is easy

---
+++ polymorphism/main.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class main {
+    public static void main(String[] args) {
+        Shapes shape= new Shapes();
+        Circle circle= new Circle();
+        triangle trianglee= new triangle();
+
+        shape.area();
+        circle.area();
+        trianglee.area();
+        
+    }
+}
